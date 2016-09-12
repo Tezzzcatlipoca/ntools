8f0a6f3fd859227e02b8f820f18ac97de69c2d9c
Franco <robertoe.francoa@gmail.com>
2016-09-12 17:20:46 -0500

    Agrega funcion que devuelve los universos y tots previos (ademas de los ultimos)

--- UltimasBases.R
+++ UltimasBases.R
@@ -8,6 +8,11 @@ DirTots<-read.table("C:/Users/franro04/Documents/R Code/DirectorioTots.txt",sep=
 as.character(DirTots[DirTots$Ind==indice,'Completo'][1])
 }
 
+previotot<-function(indice) {
+     DirTots<-read.table("C:/Users/franro04/Documents/R Code/DirectorioTots.txt",sep="\t",header = TRUE)
+     as.character(DirTots[DirTots$Ind==indice,'Completo'][2])
+}
+
 ultimouniv<-function(enh=TRUE) {
 direcUnis<-read.table("C:/Users/franro04/Documents/R Code/DirectorioUnis.txt",sep="\t",header = TRUE)
 if (enh==TRUE) { 
@@ -18,3 +23,12 @@ if (enh==TRUE) {
 as.character(direcUnis[direcUnis$Enh==cual,'Complete'][1])
 }
 
+previouniv<-function(enh=TRUE) {
+     direcUnis<-read.table("C:/Users/franro04/Documents/R Code/DirectorioUnis.txt",sep="\t",header = TRUE)
+     if (enh==TRUE) { 
+          cual<-'Y' 
+     } else {
+          cual<-'N'
+     }
+     as.character(direcUnis[direcUnis$Enh==cual,'Complete'][2])
+}
