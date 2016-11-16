de09a1a41f2817c2ff464ad08cbe5650cc41de35
Franco <robertoe.francoa@gmail.com>
2016-11-16 15:38:32 -0600

    Agrega funcion para extraer variables de la muestra maestra como en SQL

--- ExtraeDeSMS.R
+++ ExtraeDeSMS.R
@@ -56,3 +56,39 @@ ips<-function(index_id,variables,args=NA){
      close(smsh)
      out
 }
+
+sm<-function(variables,args=NA){
+     library(RODBC)
+     sms<-odbcConnect('sms','nretail','nretail')
+     varos<-paste0(variables,collapse = ", ")
+     if (!is.na(args)) {
+          linea<-paste0("SELECT ",varos," FROM source_master WHERE ",args)
+     } else {
+          linea<-paste0("SELECT ",varos," FROM source_master")
+     }
+     out<-sqlQuery(sms,linea)
+     close(sms)
+     out
+}
+
+mm<-function(Enh=1,variables,args=NA){
+     if(Enh==1 & !exists('uniEnh15')){
+          uniEnh15<<-read.csv(ultimouniv())
+     } else if (Enh==0 & !exists('uni')) {
+          uni<<-read.csv(ultimouniv(FALSE))          
+     } else if (Enh>1 || Enh<0){
+          stop('Error. El valor de Enh debe ser 0 o 1.')
+     }
+     
+     if (Enh==1) {nome<-'uniEnh15'} else {nome<-'uni'}
+     
+     library(sqldf)
+     varos<-paste0(variables,collapse = ", ")
+     if (!is.na(args)) {
+          linea<-paste0("SELECT ",varos," FROM ",nome," WHERE ",args)
+     } else {
+          linea<-paste0("SELECT ",varos," FROM ",nome)
+     }
+     out<-sqldf(linea)
+     out
+}
