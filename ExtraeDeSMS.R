e99e94d59e225f8bb0ee89f2b2859c96c8af0fc1
Franco <robertoe.francoa@gmail.com>
2016-11-28 18:22:23 -0600

    Agrega funciones para extraer informacion de mercados

--- ExtraeDeSMS.R
+++ ExtraeDeSMS.R
@@ -1,4 +1,48 @@
 
+ipm<-function(index_id,variables,args=NA){
+     library(RODBC)
+     smsh<-odbcConnect('smsh','nretail','nretail')
+     varos<-paste0(variables,collapse = ", ")
+     per<-ult.periodo(index_id)
+     if (!is.na(args)) {
+          linea<-paste0("SELECT ",varos," FROM index_period_market WHERE status_id IN (2) AND index_id = ",index_id, " AND period_id = ",per," AND ",args)
+     } else {
+          linea<-paste0("SELECT ",varos," FROM index_period_market wHERE status_id IN (2) AND index_id = ",index_id, " AND period_id = ",per)
+     }
+     out<-sqlQuery(smsh,linea)
+     close(smsh)
+     out
+}
+
+ipmcur<-function(index_id,variables,args=NA){
+     library(RODBC)
+     sms<-odbcConnect('sms','nretail','nretail')
+     varos<-paste0(variables,collapse = ", ")
+     if (!is.na(args)) {
+          linea<-paste0("SELECT ",varos," FROM index_period_market WHERE status_id IN (2) AND index_id = ",index_id," AND ",args)
+     } else {
+          linea<-paste0("SELECT ",varos," FROM index_period_market wHERE status_id IN (2) AND index_id = ",index_id)
+     }
+     out<-sqlQuery(sms,linea)
+     close(sms)
+     out
+}
+
+ipm_raw<-function(index_id,variables,args=NA){
+     library(RODBC)
+     smsh<-odbcConnect('smsh','nretail','nretail')
+     varos<-paste0(variables,collapse = ", ")
+     #per<-ult.periodo(index_id)
+     if (!is.na(args)) {
+          linea<-paste0("SELECT ",varos," FROM index_period_market WHERE status_id IN (2) AND index_id = ",index_id, " AND ",args)
+     } else {
+          linea<-paste0("SELECT ",varos," FROM index_period_market wHERE status_id IN (2) AND index_id = ",index_id)
+     }
+     out<-sqlQuery(smsh,linea)
+     close(smsh)
+     out
+}
+
 ipccur<-function(index_id,variables,args=NA){
      library(RODBC)
      sms<-odbcConnect('sms','nretail','nretail')
@@ -42,6 +86,21 @@ ipc<-function(index_id,variables,args=NA){
      out
 }
 
+ipc_raw<-function(index_id,variables,args=NA){
+     library(RODBC)
+     smsh<-odbcConnect('smsh','nretail','nretail')
+     varos<-paste0(variables,collapse = ", ")
+     #per<-ult.periodo(index_id)
+     if (!is.na(args)) {
+          linea<-paste0("SELECT ",varos," FROM index_period_cell WHERE status_id IN (2) AND index_id = ",index_id, " AND ",args)
+     } else {
+          linea<-paste0("SELECT ",varos," FROM index_period_cell wHERE status_id IN (2) AND index_id = ",index_id)
+     }
+     out<-sqlQuery(smsh,linea)
+     close(smsh)
+     out
+}
+
 ips<-function(index_id,variables,args=NA){
      library(RODBC)
      smsh<-odbcConnect('smsh','nretail','nretail')
@@ -51,6 +110,21 @@ ips<-function(index_id,variables,args=NA){
           linea<-paste0("SELECT ",varos," FROM index_period_source WHERE status_id IN (6,7,8,9) AND index_id = ",index_id," AND period_id = ",per," AND ",args)
      } else {
           linea<-paste0("SELECT ",varos," FROM index_period_source wHERE status_id IN (6,7,8,9) AND index_id = ",index_id," AND period_id = ",per)
+     }
+     out<-sqlQuery(smsh,linea)
+     close(smsh)
+     out
+}
+
+ips_raw<-function(index_id,variables,args=NA){
+     library(RODBC)
+     smsh<-odbcConnect('smsh','nretail','nretail')
+     #per<-ult.periodo(index_id)
+     varos<-paste0(variables,collapse = ", ")
+     if (!is.na(args)) {
+          linea<-paste0("SELECT ",varos," FROM index_period_source WHERE status_id IN (6,7,8,9) AND index_id = ",index_id," AND ",args)
+     } else {
+          linea<-paste0("SELECT ",varos," FROM index_period_source wHERE status_id IN (6,7,8,9) AND index_id = ",index_id)
      }
      out<-sqlQuery(smsh,linea)
      close(smsh)
